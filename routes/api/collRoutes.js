00886681f3cc994add9dae8dc1df970bd72d99a4
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-07-01 14:56:59 -0700

    update

--- routes/api/collRoutes.js
+++ routes/api/collRoutes.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const apiController = require("../../controller/controller");
+const jwtMiddleware = require("../../lib/jwtMiddleware");
 
 router
   .route("/")
-  .post(apiController.addCollectible);
+  .post(jwtMiddleware, apiController.addCollectible);
 
 router
   .route("/:id")
-  .get(apiController.getCollectible)
-  .put(apiController.updateCollectible)
-  .delete(apiController.deleteCollectible);
+  .get(jwtMiddleware, apiController.getCollectible)
+  .put(jwtMiddleware, apiController.updateCollectible)
+  .delete(jwtMiddleware, apiController.deleteCollectible);
 
   module.exports = router;
